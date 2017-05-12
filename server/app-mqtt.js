ca9ad82030e2c753f80cb8c99eb8b2364bb3b471
Felipe Berton <fefb08@gmail.com>
2017-05-12 15:53:49 -0300

    testin termux-api node

--- server/app-mqtt.js
+++ server/app-mqtt.js
@@ -1,7 +1,15 @@
 import{RxMqtt} from 'reactivex-mqtt';
-
+var api = require('termux-api').default;
 
 const PORT = 1883; // The Standard port for MQTT
 const LOCAL = 'tcp://192.168.15.4:' + PORT;
 
 var client = new RxMqtt(LOCAL);
+
+
+api.createCommand()
+    .toast()
+    .setText('Can you see me?')
+    .shortDuration()
+    .build()
+    .run();
