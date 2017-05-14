caa168246e07bc7fbac3b165484798b430e510db
Felipe Berton <fefb08@gmail.com>
2017-05-14 20:07:59 -0300

    test

--- Test/CycleTest.js
+++ Test/CycleTest.js
@@ -49,7 +49,7 @@ export class CycleTest {
       fields: fields
     });
 
-    fs.writeFile('csv/file.csv', csv, function(err) {
+    fs.writeFile('../storage/file.csv', csv, function(err) {
       if (err)
         throw err;
       console.log('file saved');
